72305148440e852e63bb3b32768381b216bdf173
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-04-30 18:03:24 -0500

    add functions to return the size of list, first node, and last node

--- linked-list.js
+++ linked-list.js
@@ -15,6 +15,7 @@ class LinkedList {
 
         if (!this.head) {
             this.head = node;
+            
         } else {
             let current = this.head;
 
@@ -24,6 +25,45 @@ class LinkedList {
 
             current.next = node;
         }
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+
+        } else {
+            let replacedHead = this.head;
+            this.head = newNode;
+            newNode.next = replacedHead;
+        }
+    }
+
+    getSize() {
+        let size = 0;
+        let current = this.head;
+
+        while (current) {
+            size++;
+            current = current.next;
+        }
+
+        return `This list has ${size} nodes`; 
+    }
+
+    getHead() {
+        return `The first node is ${this.head.value}`;
+    }
+
+    getTail() {
+        let current = this.head;
+
+        while(current.next) {
+            current = current.next;
+        }
+
+        return `The last node is ${current.value}`;
     }
 
     toString() {
@@ -41,9 +81,15 @@ class LinkedList {
 }
 
 const myList = new LinkedList();
+
 myList.append(5);
 myList.append(10);
 myList.append(15);
-myList.append(21);
+
+myList.prepend(21);
+myList.prepend(1);
 
 console.log(myList.toString());
+console.log(myList.getSize());
+console.log(myList.getHead());
+console.log(myList.getTail());
