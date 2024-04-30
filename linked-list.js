56535b83dee8d7d8c6a767d86e9cb3f59c74827f
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-04-30 17:20:53 -0500

    add append() - add node to end of list; add toString() - logs entire list in console

--- linked-list.js
+++ linked-list.js
@@ -9,4 +9,41 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    append(value) {
+        const node = new Node(value);
+
+        if (!this.head) {
+            this.head = node;
+        } else {
+            let current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+    }
+
+    toString() {
+        let wholeList = '';
+        let current = this.head;
+
+        while (current) {
+            wholeList += `(${current.value}) -> `;
+            current = current.next;
+        }
+
+        wholeList += `null`;
+        return wholeList.trim();
+    }
 }
+
+const myList = new LinkedList();
+myList.append(5);
+myList.append(10);
+myList.append(15);
+myList.append(21);
+
+console.log(myList.toString());
